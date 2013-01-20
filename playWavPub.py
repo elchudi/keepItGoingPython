d2a81d2cb33ce925e827261a474cd1fa6c78b1df
ggiudici <gustavog@elchudi.xen.prgmr.com>
2013-01-20 07:51:15 +0000

    naive implementation

--- playWavPub.py
+++ playWavPub.py
@@ -1,7 +1,10 @@
+from __future__ import division
 import pyaudio
 import wave
 import sys
 import zmq
+import ntplib
+from time import ctime
 
 context   = zmq.Context(1)
 publisher = context.socket(zmq.PUB)
@@ -13,7 +16,7 @@ def play(filepath):
     chunk = 1024
     wf = wave.open(filepath, 'rb')
     p = pyaudio.PyAudio()
-
+    rate = wf.getframerate()
     # open stream
     stream = p.open(format =
                     p.get_format_from_width(wf.getsampwidth()),
@@ -25,12 +28,22 @@ def play(filepath):
     #data = wf.readframes(chunk)
     data = True
     # play stream
+    i = 0;
+    #c = ntplib.NTPClient()
+    #response = c.request('nist1-ny.ustiming.org', version=3)
+    #stime = int(response.orig_time * 1000)
+    #print stime
+    #publisher.send_multipart(["S", str(stime)])
     while data != '':
+        i = i + 1
         #print wf.tell()
         data = wf.readframes(chunk)
         stream.write(data)
-        publisher.send_multipart(["B", str(wf.tell()-chunk)])
-
+        
+        if(not i % 223):
+            publisher.send_multipart(["B", str(wf.tell()-chunk)])
+            publisher.send_multipart(["SECS", str((wf.tell()-chunk)/rate)])
+       
     stream.close()
     p.terminate()
 
